8280cfa3bcbde9f2fc3df8d3178ebb8df98a8e23
juliano <julianojd@ymail.com>
2023-01-26 14:54:06 -0300

    Added feature to erase all items of keychains app

--- dummyApp/Features/Home/Views/HomeView.swift
+++ dummyApp/Features/Home/Views/HomeView.swift
@@ -21,13 +21,23 @@ final class HomeView: UIView {
         button.addTarget(self, action: #selector(showLogin), for: .touchUpInside)
         return button
     }()
+    
+    let deleteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Apagar todas as chaves", for: .normal)
+        button.backgroundColor = .red
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        button.addTarget(self, action: #selector(deleteAllItems), for: .touchUpInside)
+        return button
+    }()
 
     
     let stackButton: UIStackView = {
        let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.spacing = 8
+        stackView.spacing = 16
         return stackView
     }()
     
@@ -54,26 +64,46 @@ extension HomeView {
     func showLogin() {
         navigation.pushViewController(LoginViewController(navigation: navigation), animated: true)
     }
+    
+    @objc
+    func deleteAllItems() {
+        
+        let alertController = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        if let error = KeychainHelper.deleteAll() {
+            alertController.title = "Erro"
+            alertController.message = error.localizedDescription
+        } else {
+            alertController.title = "Sucesso"
+            alertController.message = "Todas as chaves foram apagadas"
+        }
+        
+        navigation.topViewController?.present(alertController, animated: true)
+    }
 }
 
 extension HomeView: ViewCodingProtocol {
     func buildViewHierarchy() {
         stackButton.addArrangedSubview(createButton)
         stackButton.addArrangedSubview(loginButton)
+        stackButton.addArrangedSubview(deleteButton)
         addSubview(stackButton)
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            stackButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            stackButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            stackButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackButton.centerXAnchor.constraint(equalTo: centerXAnchor),
             stackButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             stackButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
         ])
         
+        let buttonHeight = CGFloat(48)
         NSLayoutConstraint.activate([
-            createButton.heightAnchor.constraint(equalToConstant: 44),
-            loginButton.heightAnchor.constraint(equalToConstant: 44),
+            createButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            loginButton.heightAnchor.constraint(equalToConstant: buttonHeight),
+            deleteButton.heightAnchor.constraint(equalToConstant: buttonHeight),
         ])
     }
     
